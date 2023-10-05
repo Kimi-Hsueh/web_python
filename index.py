41ff192d4483ed8679b5aeb624335bcdb8bc8774
Kimi-Hsueh <kimi80505@gmail.com>
2023-10-05 12:24:22 +0800

    套用reder_template及指定index首頁位置

--- index.py
+++ index.py
@@ -1,13 +1,9 @@
-from flask import Flask #import flask的套件
+from flask import Flask, render_template #import flask與render_template的套件
 
 #flask的啟動指令：flask --app py檔名 run --debug
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-@app.route('/second')
-def second():
-    return "<p>second page</p>"
+def index():
+    return render_template("index.jinja.html")
